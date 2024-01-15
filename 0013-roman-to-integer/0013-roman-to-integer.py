65ae0d71d55b4858835b49705c9cd36aee0ea0d0
m-memon <153302152+m-memon@users.noreply.github.com>
2024-01-15 05:24:08 +0500

    Time: 35 ms (38.19%), Space: 13.4 MB (52.14%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        my_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        result = my_map[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if my_map[s[i]] < my_map[s[i + 1]]:
+                result -= my_map[s[i]]
+            else:
+                result += my_map[s[i]]
+
+        return result
