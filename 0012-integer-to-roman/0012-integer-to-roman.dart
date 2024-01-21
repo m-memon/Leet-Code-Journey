20195e97371b6d4fca6dd80cfc67aa4a718241f3
m-memon <153302152+m-memon@users.noreply.github.com>
2024-01-21 19:38:51 +0500

    Time: 464 ms (35.38%), Space: 153.3 MB (10.77%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.dart
@@ -0,0 +1,14 @@
+class Solution {
+  String intToRoman(int num) {
+    String roman = "";
+    List <String> notations = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    List <int> value = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    for (int pos = 0; num > 0; ++pos) {
+      while (num >= value[pos]) {
+        roman += notations[pos];
+        num -= value[pos];
+      }
+    }
+    return roman;
+  }
+}
