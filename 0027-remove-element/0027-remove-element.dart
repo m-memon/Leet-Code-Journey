05a2e4a195fb7493e2f0c1d33f130c0247df0441
m-memon <153302152+m-memon@users.noreply.github.com>
2024-02-04 13:02:40 +0500

    Time: 320 ms (62.20%), Space: 152.2 MB (6.43%) - LeetHub

--- 0027-remove-element/0027-remove-element.dart
+++ 0027-remove-element/0027-remove-element.dart
@@ -1,7 +1,6 @@
 class Solution {
   int removeElement(List<int> nums, int val) {
     nums.removeWhere((element) => element == val);
-    print("Returned List: $nums");
     return nums.length;
   }
 }
