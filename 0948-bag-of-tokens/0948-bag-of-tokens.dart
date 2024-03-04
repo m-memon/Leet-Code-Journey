118342781af517a42b8170eb9291216d8b8e871f
m-memon <153302152+m-memon@users.noreply.github.com>
2024-03-04 22:17:03 +0500

    Time: 369 ms (100.00%), Space: 155.2 MB (100.00%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.dart
@@ -0,0 +1,28 @@
+import 'dart:collection';
+
+class Solution {
+  int bagOfTokensScore(List<int> tokens, int power) {
+    int ans = 0;
+    int score = 0;
+    int i = 0;                  // the index of the smallest token
+    int j = tokens.length - 1;  // the index of the largest token
+
+    tokens.sort();
+
+    while (i <= j && (power >= tokens[i] || score > 0)) {
+      while (i <= j && power >= tokens[i]) {
+        // Play the smallest face up.
+        power -= tokens[i++];
+        score++;
+      }
+      ans = ans > score ? ans : score;
+      if (i <= j && score > 0) {
+        // Play the largest face down.
+        power += tokens[j--];
+        score--;
+      }
+    }
+
+    return ans;
+  }
+}
